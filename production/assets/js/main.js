19ba7f44289e112b37c89d4d78dc171b61f60dc0
Andres Gomez <ango848@gmail.com>
2014-09-29 00:02:08 -0500

    commit

--- production/assets/js/main.js
+++ production/assets/js/main.js
@@ -1,10 +1,30 @@
+
 $(document).ready(function() {
+    $(".chosen").chosen()
+
+        $('#inmuebles').click(function(event) {
+        $.scrollTo('#inmuebles-anchor', 600, {
+            easing: 'easeOutCirc',
+            offset: 40
+        });
+    });
+    $('#proyectos').click(function(event) {
+        $.scrollTo('#proyectos-anchor', 600, {
+            easing: 'easeOutCirc',
+            offset: -10
+        });
+    });
+        $('#contacto').click(function(event) {
+        $.scrollTo('#contacto-anchor', 600, {
+            easing: 'easeOutCirc',
+            offset: -10
+        });
+    });
 
 
 });
 
-Drupal.behaviors.myBehavior = {
-  attach: function (context, settings) {
 
 
-});
+
+
